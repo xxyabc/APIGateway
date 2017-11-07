8bde601af74c2d69fb5003d282e864eab4d4887d
xxyabc <xxy950518>
2017-11-07 08:33:27 +0800

    编排注册并生成导出YAML文件

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,8 @@
-var apiGatewayCtrls = angular.module('apiGatewayCtrls', []);
+var apiGatewayCtrls = angular.module('apiGatewayCtrls', ['ionic']);
 
-apiGatewayCtrls.controller('StartCtrl', ['$scope',
-    function($scope) {
+apiGatewayCtrls.controller('StartCtrl', ['$scope','$http',
+    function($scope,$http) {
+
         $scope.file = '文件';
         $scope.debug = '调试';
         $scope.tools = '工具';
@@ -9,6 +10,7 @@ apiGatewayCtrls.controller('StartCtrl', ['$scope',
         $scope.help = '帮助';
         $scope.APIproperty = 'API属性';
         $scope.XMLfile = 'XML文件';
+        $scope.YAMLfile = 'YAML文件';
         $scope.property = '属性';
         $scope.ID = 'ID';
         $scope.name = '名称';
@@ -17,15 +19,13 @@ apiGatewayCtrls.controller('StartCtrl', ['$scope',
         $scope.out = '输出';
         $scope.URL = 'URL';
         $scope.information = '调试信息';
-        $scope.debug1 = '运行';
+        $scope.debug1 = '注册';
         $scope.debug2 = '调试';
         $scope.debug3 = '生成XML';
         $scope.debug4 = 'XML文件导出';
-        // $scope.items =[
-        //  {title:"运行"},
-        //  {title:"调试"},
-        //  {title:"生成XML"}
-        // ];
+        $scope.debug5 = '转YAML';
+        $scope.debug6 = 'YAML文件导出';
+        $scope.items =['1','2','3'];
         $scope.toolss =[
          {title:"工具1"},
          {title:"工具2"},
@@ -36,23 +36,76 @@ apiGatewayCtrls.controller('StartCtrl', ['$scope',
          {title:"文件2"},
          {title:"文件3"}
         ];
-        $scope.newxml = function(){
+        $scope.newxml = function(){//生成xml
             var xml = Blockly.Xml.workspaceToDom(workspace);
             var xml_text = Blockly.Xml.domToText(xml);
             //alert(xml_text);
             $scope.xml = xml_text;
         };
 
-        $scope.newfile = function(){
-            var txtdata = $scope.xml;
+        $scope.newfile = function(){//导出yaml
+            var txtdata = $scope.yaml;
             //alert(txtdata);
             var content = ""+txtdata;
             var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
-            saveAs(blob, "xmlfile.xml");//saveAs(blob,filename) //仅限于chorme的下载目录里
+            saveAs(blob, "file.yaml");//saveAs(blob,filename) //仅限于chorme的下载目录里
+        };
+
+        $scope.newyaml = function(data){
+                $scope.yaml=data;
         };
 
     }
-]);
+]).controller('PopupCtrl',function($scope,$http, $ionicPopup, $timeout) {
+
+         // Triggered on a button click, or some other target
+         $scope.showPopup = function() {
+           $scope.data = {}
+           var myPopup = $ionicPopup.show({
+             template: '<input type="text" ng-model="data.wifi">',
+             title: 'API注册',
+             subTitle: '请输入注册API名称',
+             scope: $scope,
+             buttons: [
+               { text: '取消' },
+               {
+                 text: '<b>注册</b>',
+                 type: 'button-positive',
+                 onTap: function(e) {
+                   if (!$scope.data.wifi) {
+                     // 不允许用户关闭，除非输入 wifi 密码
+                     e.preventDefault();
+                   } else {
+                    //alert($scope.data.wifi);
+                    //生成xml
+                     var xml = Blockly.Xml.workspaceToDom(workspace);
+                     var xml_text = Blockly.Xml.domToText(xml);
+                     //alert(xml_text);
+                     //xml+apiname后送至后台
+                     $http({  
+                         method  : 'post',  
+                         url     : 'http://www.linyimin.club:8001/combination/getFlowXML',  
+                         params    :{"fileContent":xml_text,"serviceName":$scope.data.wifi},  // 传递数据作为字符串，从前台传到后台  
+                     }).success(function(data,status,headers,config) {//这里的data，就是后台传递过来的数据jsonArray  
+                        // alert("yes!!!");
+                        alert("注册成功！");
+                        $scope.newyaml(data);
+                     }).error(function(data,status,headers,config){  
+                          alert("错误");  
+                     }); 
+                   }
+                 }
+               },
+             ]
+           });
+           // myPopup.then(function(res) {
+           //   console.log('Tapped!', res);
+           // });
+           // $timeout(function() {
+           //    myPopup.close(); // 3秒后关闭弹窗
+           // }, 3000);
+          };
+        });
 
 apiGatewayCtrls.controller('HelloCtrl', ['$scope',
     function($scope) {
